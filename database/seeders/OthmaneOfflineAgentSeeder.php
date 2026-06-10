956b3593c5b654a9d02ad074f4bcda79eb08ba62
Oussama <votre.email@example.com>
2026-06-10 12:14:27 +0100

    v3 workspace reservations

--- database/seeders/OthmaneOfflineAgentSeeder.php
+++ database/seeders/OthmaneOfflineAgentSeeder.php
@@ -23,6 +23,7 @@ class OthmaneOfflineAgentSeeder extends Seeder
 
         $permissions = [
             'custom_requests.view',
+            'custom_requests.create',
             'custom_requests.quote',
             'custom_requests.documents',
         ];
